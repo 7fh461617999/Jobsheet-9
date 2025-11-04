da5f155e5d9ad51c8564bf0aa0cd8d98e751fae4
habibi <habibifirman117@gmail.com>
2025-11-04 09:44:56 +0700

    modify experiment 1

--- experiment/ArrayNumbers11.java
+++ experiment/ArrayNumbers11.java
@@ -2,11 +2,10 @@ package experiment;
 
 public class ArrayNumbers11 {
     public static void main(String[] args) {
-        int[] num  = {5,12,7,20};
+        int[] num = { 5, 12, 7, 20 };
+        for (int i = 0; i < 4; i++) {
+            System.out.println(num[i]);
+        }
 
-        System.out.println(num[0]);
-        System.out.println(num[1]);
-        System.out.println(num[2]);
-        System.out.println(num[3]);
     }
 }
