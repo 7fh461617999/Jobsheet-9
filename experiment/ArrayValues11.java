acb3d5f9a8f14ea1d5c3bb032cab40578a1f795f
habibi <habibifirman117@gmail.com>
2025-11-04 09:45:14 +0700

    modify experiment 2

--- experiment/ArrayValues11.java
+++ experiment/ArrayValues11.java
@@ -6,13 +6,17 @@ public class ArrayValues11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] finalscore = new int[10];
-         for (int i = 0; i < 10; i++) {
-            System.out.print("enter the final score "+ i + ": ");
+        for (int i = 0; i < finalscore.length; i++) {
+            System.out.print("enter the final score " + i + ": ");
             finalscore[i] = sc.nextInt();
-         }
-         for (int i = 0; i < 10; i++) {
-            System.out.println("final score "+i+" :" + finalscore[i]);
-         }
-         sc.close();
+        }
+        for (int i = 0; i < 10; i++) {
+            if (finalscore[i] > 70) {
+                System.out.println("final score " + i + " : passed!");
+            }else {
+                System.out.println("final score "+i+" : failed!");
+            }
+        }
+        sc.close();
     }
 }
