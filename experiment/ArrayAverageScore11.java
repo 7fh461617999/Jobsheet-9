170a8cd556b1719b6b1f52ee29be031deea2fe7f
habibi <habibifirman117@gmail.com>
2025-11-04 08:20:23 +0700

    First commit Experiment 3

---
+++ experiment/ArrayAverageScore11.java
@@ -0,0 +1,20 @@
+package experiment;
+import java.util.Scanner;
+public class ArrayAverageScore11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] score = new int[10];
+        double total = 0;
+        double average ;
+        for (int i = 0; i < score.length; i++) {
+            System.out.print("enter student score "+ i +": ");
+            score[i]=sc.nextInt();
+        }
+        for (int i = 0; i < score.length; i++) {
+            total += score[i]; 
+        }
+        average = total/score.length;
+        System.out.println("the class average score is : "+average);
+        sc.close();
+    }
+}
